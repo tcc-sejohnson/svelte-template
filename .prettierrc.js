fe996d52d8aa6f19754ce948f05cba02b01e6e0a
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2021-04-20 14:05:00 -0600

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 120,
+    trailingComma: "es5",
+    singleQuote: true,
+    tabWidth: 2,
+    plugins: ["prettier-plugin-svelte"],
+    svelteSortOrder: 'options-styles-scripts-markup',
+    svelteStrictMode: false,
+    svelteBracketNewLine: true,
+    svelteIndentScriptAndStyle: true,
+}
